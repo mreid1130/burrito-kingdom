c06b965decf9a03917fe463a10fb1e1b4c706ad9
Mark Reid <mark.francis.reid@gmail.com>
2015-09-05 20:57:12 -0700

    adds dependencies and sets up chokidar watchers

---
+++ index.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var chokidar = require('chokidar');
+
+module.exports.burritoWatch = function(directory) {
+  var watcher = chokidar.watch(directory, {
+    ignored: /[\/\\]\./,
+    persistent: true
+  });
+
+  watcher
+    .on('add', function(path, stats) {
+      log('File', path, 'has been added');
+    });
+
+  watcher
+    .on('change', function(path, stats) {
+      log('File', path, 'has been added');
+    });
+
+
+};
+
+// no need for cron for nows
+// var CronJob = require('cron').CronJob;
+// var job = new CronJob('00 */10 * * * *', function() {
+//   console.log('running cron jobs!!!!');
+// }, function() {
+//   console.log('done');
+// });
