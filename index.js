eeb6871a173764e8b75380ed430997de7894aa04
Mark Reid <mark.francis.reid@gmail.com>
2015-09-26 10:47:21 -0700

    removes inline ifs

--- index.js
+++ index.js
@@ -9,20 +9,28 @@ module.exports.burritoWatch = function(directory) {
   var getFiles = function(dir, done) {
     var results = [];
     fs.readdir(dir, function(err, list) {
-      if (err) return done(err);
+      if (err) {
+        return done(err);
+      }
       var pending = list.length;
-      if (!pending) return done(null, results);
+      if (!pending) {
+        return done(null, results);
+      }
       list.forEach(function(file) {
         file = path.resolve(dir, file);
         fs.stat(file, function(err, stat) {
           if (stat && stat.isDirectory()) {
             getFiles(file, function(err, res) {
               results = results.concat(res);
-              if (!--pending) done(null, results);
+              if (!--pending) {
+                done(null, results);
+              }
             });
           } else {
             results.push(file);
-            if (!--pending) done(null, results);
+            if (!--pending) {
+              done(null, results);
+            }
           }
         });
       });
