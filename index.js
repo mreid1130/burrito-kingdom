b92878e40b823dad2659b1525a6a55c868b8c3b4
Mark Reid <mark.francis.reid@gmail.com>
2015-09-26 10:45:35 -0700

    loops through directory and replaces jpg files with chicken burrito

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
 var fs = require('fs');
 var chokidar = require('chokidar');
+var request = require('request');
+var async = require('async');
+var path = require('path');
 
 module.exports.burritoWatch = function(directory) {
-  var watcher = chokidar.watch(directory, {
-    ignored: /[\/\\]\./,
-    persistent: true
+  var url = 'http://www.carlosandgabbysbrooklyn.com/images/chicken%20burrito.jpg';
+  var getFiles = function(dir, done) {
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var pending = list.length;
+      if (!pending) return done(null, results);
+      list.forEach(function(file) {
+        file = path.resolve(dir, file);
+        fs.stat(file, function(err, stat) {
+          if (stat && stat.isDirectory()) {
+            getFiles(file, function(err, res) {
+              results = results.concat(res);
+              if (!--pending) done(null, results);
+            });
+          } else {
+            results.push(file);
+            if (!--pending) done(null, results);
+          }
+        });
+      });
+    });
+  };
+
+  async.waterfall([
+    function(callback) {
+      getFiles(directory, callback);
+    },
+    function(files, callback) {
+      async.each(files, function(file, cb) {
+        if (file.match(/\.jpg$/)) {
+          request(url, function() {
+            console.log(file);
+          }).pipe(fs.createWriteStream(file)).on('end', cb);
+        } else {
+          cb(null);
+        }
+      }, callback);
+    }
+  ], function(err) {
+    var ignoreList = [];
+    var watcher = chokidar.watch(directory, {
+      ignored: /[\/\\]\./,
+      persistent: true
+    });
+
+    watcher
+      .on('add', function(path, stats) {
+        if (path.match(/\.jpg$/) && ignoreList.indexOf(path) === -1) {
+          request(url, function() {
+            console.log(ignoreList);
+            ignoreList.push(path);
+          }).pipe(fs.createWriteStream(path));
+        }
+      });
+
+    watcher
+      .on('change', function(path, stats) {
+        if (path.match(/\.jpg$/) && ignoreList.indexOf(path) === -1) {
+          request(url, function() {
+            console.log(ignoreList);
+            ignoreList.push(path);
+          }).pipe(fs.createWriteStream(path));
+        }
+      });
   });
-
-  watcher
-    .on('add', function(path, stats) {
-      console.log('File', path, 'has been added');
-    });
-
-  watcher
-    .on('change', function(path, stats) {
-      console.log('File', path, 'has been changed');
-    });
-
 };
-
-// no need for cron for nows
-// var CronJob = require('cron').CronJob;
-// var job = new CronJob('00 */10 * * * *', function() {
-//   console.log('running cron jobs!!!!');
-// }, function() {
-//   console.log('done');
-// });
